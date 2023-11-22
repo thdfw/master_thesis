69ea03e91e8e39bc48dab54317e7fc10caff45f5
thdfw <91254985+thdfw@users.noreply.github.com>
2023-11-21 19:05:19 -0800

    Moved stuff to plot.py and improved plot

--- gridworks/master.py
+++ gridworks/master.py
@@ -8,7 +8,7 @@ import optimizer, functions, plot, forecasts
 # ------------------------------------------------------
 
 # Simulation time
-num_iterations = 150
+num_iterations = 4
 
 # Horizon
 N = 10
@@ -20,23 +20,8 @@ pb_type = {
 'horizon':          N
 }
 
-# ------------------------------------------------------
-# Print
-# ------------------------------------------------------
-
-# Linearized or not
-if pb_type['linearized']: print("\nProblem type: Linearized")
-else: print("\nProblem type: Non-linear")
-
-# Variables: mixed integer or continuous
-if pb_type['mixed-integer']: print("Variables: Mixed-Integer")
-else: print("Variables: Continuous")
-
-# Solver: gurobi or ipopt
-if pb_type['gurobi']: print("Solver: Gurobi")
-else:
-    print("Solver: Ipopt")
-    if pb_type['mixed-integer']: raise ValueError("Change the solver to Gurobi to solve the MIP.")
+# Print corresponding setup
+plot.print_pb_type(pb_type)
 
 # ------------------------------------------------------
 # Initial state of the system
@@ -49,8 +34,9 @@ x_0 = [300.0]*4 + [320.0]*12
 a = [330, 0.25] + [0.6]*4 + x_0
 
 # For plots
-list_Q_HP = []
-list_S11, list_S21, list_S31 = [x_0[4]], [x_0[8]], [x_0[12]]
+list_Q_HP, list_S11, list_S21, list_S31 = [], [x_0[4]], [x_0[8]], [x_0[12]]
+delta_t_h = 2/60
+elec_cost, elec_used = 0, 0
 
 # ------------------------------------------------------
 # MPC
@@ -60,6 +46,8 @@ for iter in range(num_iterations):
 
     # Get u* and x*
     u_opt, x_opt = optimizer.optimize_N_steps(x_0, a, iter, pb_type)
+    
+    # Extract u0* and x0
     u_opt_0 = [round(float(x),6) for x in u_opt[:,0]]
     x_opt_0 = [round(float(x),6) for x in x_opt[:,0]]
 
@@ -72,46 +60,36 @@ for iter in range(num_iterations):
     # Update "a", the point around which we linearize
     a = u_opt_0[:2] + [0.6]*4 + x_1
 
-    # ------------------------------------------------------
-    # Prints
-    # ------------------------------------------------------
-    
-    S_t = "->" if round(u_opt_0[2])==0 else "<-"
-    S_b = "<-" if round(u_opt_0[2])==0 else "->"
-    B_t = "->" if round(u_opt_0[3])==1 else "<-"
-    B_b = "<-" if round(u_opt_0[3])==1 else "->"
-
-    print(f"\nBuffer {B_t} {round(x_opt[0,0],1)} | Storage  {round(x_opt[12,0],1)} {S_t}    {round(x_opt[8,0],1)} {S_t}   {round(x_opt[4,0],0)} {S_t}")
-    print(f"          {round(x_opt[1,0],1)} |          {round(x_opt[13,0],1)}       {round(x_opt[9,0],1)}      {round(x_opt[5,0],0)}")
-    print(f"          {round(x_opt[2,0],1)} |          {round(x_opt[14,0],1)}       {round(x_opt[10,0],1)}      {round(x_opt[6,0],0)}")
-    print(f"       {B_b} {round(x_opt[3,0],1)} |       {S_t} {round(x_opt[15,0],1)}    {S_t} {round(x_opt[11,0],1)}   {S_t} {round(x_opt[7,0],0)}\n")
-        
-    print(f"T_HP = {round(u_opt[0,0],1) if u_opt[4,0]==1 else '-'}, m_stor = {round(u_opt[1,0],1)}")
-        
-    print(f"\nBuffer {B_t} {round(x_1[0],1)} | Storage  {round(x_1[12],1)} {S_t}    {round(x_1[8],1)} {S_t}   {round(x_1[4],0)} {S_t}")
-    print(f"          {round(x_1[1],1)} |          {round(x_1[13],1)}       {round(x_1[9],1)}      {round(x_1[5],0)}")
-    print(f"          {round(x_1[2],1)} |          {round(x_1[14],1)}       {round(x_1[10],1)}      {round(x_1[6],0)}")
-    print(f"       {B_b} {round(x_1[3],1)} |       {S_t} {round(x_1[15],1)}    {S_t} {round(x_1[11],1)}   {S_t} {round(x_1[7],0)}")
+    # Print iteration
+    plot.print_iteration(u_opt, x_opt, x_1)
     
     # ------------------------------------------------------
     # Plots
     # ------------------------------------------------------
 
-    # Store data
-    list_Q_HP.append(functions.get_function("Q_HP", u_opt_0, x_opt_0, 0, True, False))
+    # Update total electricity use and cost
+    Q_HP = functions.get_function("Q_HP", u_opt_0, x_opt_0, 0, True, False)
+    elec_used += Q_HP/4 * delta_t_h
+    elec_cost += Q_HP/4 * delta_t_h * forecasts.get_c_el(iter,iter+1)[0]
+
+    # Append values for the
+    list_Q_HP.append(Q_HP)
     list_S11.append(x_1[4])
     list_S21.append(x_1[8])
     list_S31.append(x_1[12])
 
-# Send data to plots.py
+# Regroup the data and send it to plot
 plot_data = {
-'iterations': num_iterations,
-'c_el': forecasts.get_c_el(0,num_iterations),
-'Q_load': forecasts.get_c_el(0,num_iterations),
-'Q_HP': list_Q_HP,
-'T_S11': list_S11,
-'T_S21': list_S21,
-'T_S31': list_S31,
+    'pb_type':      pb_type,
+    'iterations':   num_iterations,
+    'c_el':         forecasts.get_c_el(0,num_iterations),
+    'Q_load':       forecasts.get_c_el(0,num_iterations),
+    'Q_HP':         list_Q_HP,
+    'T_S11':        list_S11,
+    'T_S21':        list_S21,
+    'T_S31':        list_S31,
+    'elec_cost':    round(elec_cost,2),
+    'elec_used':    round(elec_used/1000,2),
 }
 
 print("\nPlotting the data...")
--- gridworks/plot.py
+++ gridworks/plot.py
@@ -2,9 +2,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 import forecasts
 
+'''
+Print the selected problem type
+'''
+def print_pb_type(pb_type):
+
+    # Linearized or not
+    if pb_type['linearized']: print("\nProblem type: Linearized")
+    else: print("\nProblem type: Non-linear")
+
+    # Variables: mixed integer or continuous
+    if pb_type['mixed-integer']: print("Variables: Mixed-Integer")
+    else: print("Variables: Continuous")
+
+    # Solver: gurobi or ipopt
+    if pb_type['gurobi']: print("Solver: Gurobi")
+    else:
+        print("Solver: Ipopt")
+        if pb_type['mixed-integer']: raise ValueError("Change the solver to Gurobi to solve the MIP.")
+
+'''
+Prints the current iteration (x0, u0*, x1) in way that is easy to visualize
+'''
+def print_iteration(u_opt, x_opt, x_1):
+
+    u_opt_0 = [round(float(x),6) for x in u_opt[:,0]]
+
+    S_t = "->" if round(u_opt_0[2])==0 else "<-"
+    S_b = "<-" if round(u_opt_0[2])==0 else "->"
+    B_t = "->" if round(u_opt_0[3])==1 else "<-"
+    B_b = "<-" if round(u_opt_0[3])==1 else "->"
+
+    print(f"\nBuffer {B_t} {round(x_opt[0,0],1)} | Storage  {round(x_opt[12,0],1)} {S_t}    {round(x_opt[8,0],1)} {S_t}   {round(x_opt[4,0],0)} {S_t}")
+    print(f"          {round(x_opt[1,0],1)} |          {round(x_opt[13,0],1)}       {round(x_opt[9,0],1)}      {round(x_opt[5,0],0)}")
+    print(f"          {round(x_opt[2,0],1)} |          {round(x_opt[14,0],1)}       {round(x_opt[10,0],1)}      {round(x_opt[6,0],0)}")
+    print(f"       {B_b} {round(x_opt[3,0],1)} |       {S_t} {round(x_opt[15,0],1)}    {S_t} {round(x_opt[11,0],1)}   {S_t} {round(x_opt[7,0],0)}\n")
+        
+    print(f"T_HP = {round(u_opt[0,0],1) if u_opt[4,0]==1 else '-'}, m_stor = {round(u_opt[1,0],1)}")
+        
+    print(f"\nBuffer {B_t} {round(x_1[0],1)} | Storage  {round(x_1[12],1)} {S_t}    {round(x_1[8],1)} {S_t}   {round(x_1[4],0)} {S_t}")
+    print(f"          {round(x_1[1],1)} |          {round(x_1[13],1)}       {round(x_1[9],1)}      {round(x_1[5],0)}")
+    print(f"          {round(x_1[2],1)} |          {round(x_1[14],1)}       {round(x_1[10],1)}      {round(x_1[6],0)}")
+    print(f"       {B_b} {round(x_1[3],1)} |       {S_t} {round(x_1[15],1)}    {S_t} {round(x_1[11],1)}   {S_t} {round(x_1[7],0)}")
+
+'''
+The final plot with all the data accumulated during the simulation
+'''
 def plot_MPC(data):
 
     fig, ax = plt.subplots(2,1, figsize=(8,5), sharex=True)
+    
+    linearized = "Linearized" if data['pb_type']['linearized'] else "Non linear"
+    variables = "Mixed-Integer" if data['pb_type']['mixed-integer'] else "Continuous"
+    solver = "Gurobi" if data['pb_type']['gurobi'] else "Ipopt"
+    N = data['pb_type']['horizon']
+    
+    fig.suptitle(f"{linearized}, {variables}, {solver}, N={N} \nPrice: {data['elec_cost']} $, Elec: {data['elec_used']} kWh")
     
     # ------------------------------------------------------
     # First plot
