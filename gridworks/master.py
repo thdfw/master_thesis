c133f8645b1dd013a3a7ec60d3c8294d16ce3561
thdfw <91254985+thdfw@users.noreply.github.com>
2023-11-23 10:39:10 -0800

    Fixed binary NLP with removed lower bound on Q_HP

--- gridworks/master.py
+++ gridworks/master.py
@@ -43,7 +43,7 @@ a = [330, 0.25] + [0.6]*4 + x_0
 # ------------------------------------------------------
 
 list_Q_HP = []
-list_S11, list_S21, list_S31 = [x_0[4]], [x_0[8]], [x_0[12]]
+list_S11, list_S21, list_S31, list_B4 = [x_0[4]], [x_0[8]], [x_0[12]], [x_0[3]]
 delta_t_h = 2/60
 elec_cost, elec_used = 0, 0
 
@@ -107,6 +107,7 @@ for iter in range(num_iterations):
     list_S11.append(x_1[4])
     list_S21.append(x_1[8])
     list_S31.append(x_1[12])
+    list_B4.append(x_1[3])
 
 # Regroup the data and send it to plot
 plot_data = {
@@ -118,6 +119,7 @@ plot_data = {
     'T_S11':        list_S11,
     'T_S21':        list_S21,
     'T_S31':        list_S31,
+    'T_B4':         list_B4,
     'elec_cost':    round(elec_cost,2),
     'elec_used':    round(elec_used/1000,2),
 }
