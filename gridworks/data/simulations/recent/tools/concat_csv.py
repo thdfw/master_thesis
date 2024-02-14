83ac1350f3844d1a30de468f118a1b965b1ea6fe
thdfw <91254985+thdfw@users.noreply.github.com>
2024-02-14 12:27:24 -0800

    Minor updates

--- gridworks/data/simulations/recent/tools/concat_csv.py
+++ gridworks/data/simulations/recent/tools/concat_csv.py
@@ -4,12 +4,8 @@ import csv
 first_file = input("Path to first csv file: ").replace(" ","")
 second_file = input("Path to second csv file: ").replace(" ","")
 
-# Assuming df1 and df2 are your DataFrames from CSV files
 df1 = pd.read_csv(first_file)
 df2 = pd.read_csv(second_file)
 
-# Concatenate df2 to df1
 df_combined = pd.concat([df1, df2], ignore_index=True)
-
-# Save the combined DataFrame to a new CSV file
 df_combined.to_csv('appended_file.csv', index=False)
