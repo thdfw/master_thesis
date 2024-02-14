d6c2933dd8f708b37ea99850a1fad834a10e0929
thdfw <91254985+thdfw@users.noreply.github.com>
2024-02-14 11:36:44 -0800

    Minor updates

--- gridworks/sequencer.py
+++ gridworks/sequencer.py
@@ -173,7 +173,7 @@ def get_optimal_sequence(c_el, m_load, iter, previous_sequence, results_file, at
     
         # Increase the load
         print(f"\nLoad before: {load}")
-        load = [attempt*load for x in load]
+        load = [attempt*x for x in load]
         print(f"Load after: {load}\n")
 
         Q_opt, stor_opt, HP_on_off_opt, obj_opt = get_opti(N, c_el, load, max_storage, initial_storage, Q_HP_min_list, Q_HP_max_list, COP1_list)
