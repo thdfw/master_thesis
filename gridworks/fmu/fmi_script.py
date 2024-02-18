fcd38f891765594661002783f4d7d23976db84e2
thdfw <91254985+thdfw@users.noreply.github.com>
2024-02-17 19:48:18 -0800

    Added predicted and real state

--- gridworks/fmu/fmi_script.py
+++ gridworks/fmu/fmi_script.py
@@ -1,54 +1,52 @@
-from fmpy import read_model_description, dump, extract
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 18:41:04 2024
+
+@author: remi
+"""
+
+from fmpy import read_model_description, dump, extract 
 from fmpy.fmi2 import FMU2Slave
 import pandas as pd
+import os
 
-# Get the commands for the hour
-def get_commands(x_0):
-    m_stor = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    m_load = [0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11, 0.11]
-    T_sup_HP = [311.77, 311.77, 311.77, 311.77, 312.62, 313.19, 313.42, 313.5, 313.94, 314.49, 314.9, 315.13, 315.43, 315.87, 316.32]
-    delta_HP = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    return m_stor, m_load, T_sup_HP, delta_HP
 
-# Get the commands per time step
-def get_inputs(time, current_state):
-    
-    # Initialisation to get the desired initial state
-    if time<7200:
-        inputs_list = [0, 0, 310, 1]
-    # The actual commands
-    else:
-        # HERE IS WHERE YOU GET THE COMMANDS BASED ON THE CURRENT STATES => CALL MPC
-        m_stor, m_load, T_sup_HP, delta_HP = get_commands(current_state)
-        t = int((time-7200)/4/60)
-        inputs_list = [m_stor[t], m_load[t], T_sup_HP[t], delta_HP[t]]
-        inputs_list = [0, 0, 320, 1]
-        print(f"{t}: {inputs_list}")
-    #print(f"Input at {int(time/60)} min: \n{inputs_list}: ")
-    
-    return inputs_list
+# # Set the DYMOLA_RUNTIME_LICENSE environment variable
 
-# Extract the FMU
-fmu = 'plant_hp_fixed_2023.fmu'
+# license_path = r'c:/users/xiwang_lbl/appdata/roaming/dassaultsystemes/dymola/dymola.lic'
+# os.environ['DYMOLA_RUNTIME_LICENSE'] = license_path
+
+
+# Read the FMU
+
+fmu = r'C:\git\plant_hp_variable.fmu'
 model_description = read_model_description(fmu)
 unzipdir = extract(fmu)
 
+# Read the inputs and outputs from the FMU
+# To be noted, in this stage we creat a dictionary with the names of the inputs 
+# and the corresponding "address" or "ID" in the FMU
+
 vars_input = {}
-for k in model_description.modelVariables:
+for k in model_description.modelVariables: 
     if k.causality == 'input':
         vars_input[k.name] = k.valueReference
 
 vars_output = {}
-for k in model_description.modelVariables:
+for k in model_description.modelVariables: 
     if k.causality == 'output':
         vars_output[k.name] = k.valueReference
-        
+
+# Optional output that are not useful (maybe for debug ?) and that may be also 
+# not in the FMU if the correct option is selected at the export stage of the FMU
+
 delete = ['CPUtime', 'EventCounter']
 for k in delete:
     if k in vars_output.keys():
         del vars_output[k]
 
 # Initializing the model
+
 result = FMU2Slave(guid=model_description.guid,
                     unzipDirectory=unzipdir,
                     modelIdentifier=model_description.coSimulation.modelIdentifier)
@@ -62,24 +60,20 @@ result.exitInitializationMode()
 # result.setReal([list of adresses of the inputs], [list of values for those adddresses])
 
 time = 0
-step = 240 # 4 min time steps
+step = 100
 rows = []
-rows.append(result.getReal(list(vars_output.values())))
-state = rows[-1]
-
-while time < 3*3600: # 1 hour with 2 hour initialisation
-    result.setReal(vars_input.values(), get_inputs(time, state))
-    result.doStep(currentCommunicationPoint=time, communicationStepSize=step)
+while time < 2000:
+    result.setReal(vars_input.values(), [0.1, 0.2, 60 + 273.15, 1 if time < 1000 else 0])
     rows.append(result.getReal(list(vars_output.values())))
-    state = rows[-1]
+    result.doStep(currentCommunicationPoint=time, communicationStepSize=step)
     time = time + step
     
-# Terminate
+# end of the execution
+
+rows.append(result.getReal(list(vars_output.values())))
 result.terminate()
 result.freeInstance()
 
-# Read final state and print results
 res = pd.DataFrame(rows, columns = vars_output.keys())
-res = res.round(1)
-print('')
-print(res[['T[13]','T[14]','T[15]','T[16]']].loc[30:])
+
+
--- gridworks/master_fmu.py
+++ gridworks/master_fmu.py
@@ -2,7 +2,14 @@ from fmpy import read_model_description, extract
 from fmpy.fmi2 import FMU2Slave
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
 import optimizer, functions, plot, forecasts, sequencer
+
+# Set the DYMOLA_RUNTIME_LICENSE environment variable
+license_path = r'c:/users/xiwang_lbl/appdata/roaming/dassaultsystemes/dymola/dymola.lic'
+os.environ['DYMOLA_RUNTIME_LICENSE'] = license_path
 
 # ------------------------------------------------------
 # Define problem type (time step, horizon, etc.)
@@ -17,7 +24,7 @@ delta_t_s = delta_t_m*60    # seconds
 N = int(8 * 1/delta_t_h)
 
 # Simulation time (hours)
-simulation_time = 4
+simulation_time = 2
 
 # FMU initialisation time (hours)
 initialisation_time = 10
@@ -45,6 +52,16 @@ for i in range(8): previous_sequence[f'combi{i+1}'] = [1,1,1]
 # Load previous sequence results from a csv file
 file_path = input("\nResults file (enter to skip): ").replace(" ","")
 print("No file selected.") if file_path=="" else print(f"Reading from {file_path.split('/')[-1]}.")
+
+# For the results
+rows = []
+rows_pred = []
+
+'''Remove and calculate at the end'''
+# For the final plot
+list_Q_HP = []
+elec_cost, elec_used = 0, 0
+'''Remove and calculate at the end'''
 
 # ------------------------------------------------------
 # Get optimal inputs for the next hour
@@ -112,7 +129,7 @@ def get_inputs(iter, x_0, x_opt, u_opt):
 # -------------------------------------------------------
 
 # Extract the FMU file
-fmu_path = 'fmu/plant_hp_fixed_2023.fmu'
+fmu_path = 'fmu/plant_hp_fixed.fmu'
 model_description = read_model_description(fmu_path)
 unzipdir = extract(fmu_path)
 
@@ -143,16 +160,12 @@ result.setupExperiment(tolerance=1E-4, startTime=0.0)
 result.enterInitializationMode()
 result.exitInitializationMode()
 
-# Initial state
-rows = []
-rows.append(result.getReal(list(vars_output.values())))
-
 # -------------------------------------------------------
 # Initialize the FMU temperatures
 # -------------------------------------------------------
 
 # Read the initial state of the system
-state = rows[-1]
+state = result.getReal(list(vars_output.values()))
 
 # Define the initialisation commands
 initialisation_commands = [0.15, 0.15, 310, 1]
@@ -174,11 +187,13 @@ print(f"\nState after initialization:\n{[round(x,1) for x in state]}")
 # Simulate the FMU
 # -------------------------------------------------------
 
+start_time = time.time()
+
 # For each hour
-for time in range(initialisation_time, initialisation_time+simulation_time):
+for time_now in range(initialisation_time, initialisation_time+simulation_time):
 
     # The iteration in the simulation (after initialisation)
-    iter = int(time-initialisation_time)
+    iter = int(time_now-initialisation_time)
 
     # Initial solver warm start
     if iter == 0:
@@ -194,25 +209,33 @@ for time in range(initialisation_time, initialisation_time+simulation_time):
     # Apply the inputs for the 15 elements to go to the next hour
     for k in range(int(3600/delta_t_s)):
         result.setReal(vars_input.values(), optimal_inputs[f'step_{k}'])
-        result.doStep(currentCommunicationPoint=time*3600+k*delta_t_s, communicationStepSize=delta_t_s)
+        result.doStep(currentCommunicationPoint=time_now*3600+k*delta_t_s, communicationStepSize=delta_t_s)
         rows.append(result.getReal(list(vars_output.values())))
         m_stor.append(round(optimal_inputs[f'step_{k}'][0],2))
         m_load.append(round(optimal_inputs[f'step_{k}'][1],2))
         T_sup_HP.append(round(optimal_inputs[f'step_{k}'][2],2))
         delta_HP.append(optimal_inputs[f'step_{k}'][3])
     
-    # We do not give inputs for the last state
-    m_stor.append(np.nan)
-    m_load.append(np.nan)
-    T_sup_HP.append(np.nan)
-    delta_HP.append(np.nan)
+    # State predicted by equations during past hour
+    state_pred = [[round(x,1) for x in old_x[k,:15]] for k in range(16)]
+    state_pred = state_pred[4:] + state_pred[:4]
+    state_pred = list(map(list, zip(*state_pred))) # transpose the list
+    rows_pred.extend(state_pred)
 
-    # Show the evolution of the state during the past hour
-    res = pd.DataFrame(rows[-16:], columns = vars_output.keys())
+    # PRINT State predicted by equations during past hour
+    res_pred = pd.DataFrame(state_pred)
+    res_pred.columns = vars_output.keys()
+    res_pred = res_pred[['T[13]','T[14]','T[15]','T[16]']]
+    res_pred['m_stor'], res_pred['m_load'], res_pred['T_sup_HP'], res_pred['delta_HP'] = m_stor, m_load, T_sup_HP, delta_HP
+    print("\nPREDICTED Temperature evolution over the past hour:")
+    print(res_pred)
+
+    # PRINT State measured in FMU during past hour
+    res = pd.DataFrame(rows[-16:-1], columns = vars_output.keys())
     res = res.round(1)
     res = res[['T[13]','T[14]','T[15]','T[16]']]
     res['m_stor'], res['m_load'], res['T_sup_HP'], res['delta_HP'] = m_stor, m_load, T_sup_HP, delta_HP
-    print("\nTemperature evolution over the past hour:")
+    print("\nREAL Temperature evolution over the past hour:")
     print(res)
 
     # Read the next state of the system
@@ -223,6 +246,24 @@ for time in range(initialisation_time, initialisation_time+simulation_time):
     
     # Print iteration
     plot.print_iteration(old_u, old_x, state_modif, previous_sequence, 15*iter)
+    
+    # ------------------------------------------------------
+    # Update and append values for final plot
+    # ------------------------------------------------------
+
+    '''Remove and calculate at the end'''
+    # Update electricity use (kWh) and cost ($) with next hour
+    #Q_HP = m_HP * c_p * (T_sup_HP - T_ret_HP)
+    # T_ret_HP =
+    Q_HP = sum([functions.get_function("Q_HP", old_u[:,t], old_x[:,t], t, previous_sequence, 15*iter) for t in range(15)])/15
+    COP1, _ = forecasts.get_T_OA(15*iter, 15*iter+1, delta_t_h)
+    elec_used += Q_HP*COP1[0]
+    elec_cost += Q_HP*COP1[0] * forecasts.get_c_el(15*iter, 15*iter+1, delta_t_h)[0]
+
+    # Append state and Q_HP values with next hour
+    #list_B1.extend([round(float(x),6) for x in list(res['T[13]'])]) # TODO REMOVE, can compute all Q_HP and elec at the end!!!
+    list_Q_HP.extend([functions.get_function("Q_HP", old_u[:,t], old_x[:,t], 0, previous_sequence, 15*iter) for t in range(15)])
+    '''Remove and calculate at the end'''
 
 # Terminate
 result.terminate()
@@ -231,5 +272,68 @@ result.freeInstance()
 # Gather all the data from the simulation
 res = pd.DataFrame(rows, columns = vars_output.keys())
 res = res.round(1)
-#print('\nThe final results:')
-#print(res[['T[13]','T[14]','T[15]','T[16]']])
+print("\nSimulation results:")
+print(res)
+list_B1, list_B4 = list(res['T[13]']), list(res['T[16]'])
+list_S11, list_S21, list_S31 = list(res['T[1]']), list(res['T[5]']), list(res['T[9]'])
+
+# Gather all data from the prediction
+res_pred = pd.DataFrame(rows_pred)
+res_pred.columns = vars_output.keys()
+res_pred = res_pred.round(1)
+print("\nPrediction results:")
+print(res_pred)
+list_B1_pred, list_B4_pred = list(res_pred['T[13]']), list(res_pred['T[16]'])
+list_S11_pred, list_S21_pred, list_S31_pred = list(res_pred['T[1]']), list(res_pred['T[5]']), list(res_pred['T[9]'])
+
+plt.plot(list_B1, label='real')
+plt.plot(list_B1_pred, label='pred')
+plt.title("T_B1")
+plt.legend()
+plt.show()
+
+plt.plot(list_B4, label='real')
+plt.plot(list_B4_pred, label='pred')
+plt.title("T_B4")
+plt.legend()
+plt.show()
+
+plt.plot(list_S11, label='real')
+plt.plot(list_S11_pred, label='pred')
+plt.title("T_S11")
+plt.legend()
+plt.show()
+
+# ------------------------------------------------------
+# Final prints and plot
+# ------------------------------------------------------
+
+# Regroup plot data
+plot_data = {
+    'pb_type':      pb_type,
+    'iterations':   simulation_time,
+    'c_el':         forecasts.get_c_el(0, 15*simulation_time, delta_t_h),
+    'm_load':       forecasts.get_m_load(0, 15*simulation_time, delta_t_h),
+    'Q_HP':         list_Q_HP,
+    'T_S11':        list_S11,
+    'T_S21':        list_S21,
+    'T_S31':        list_S31,
+    'T_B1':         list_B1,
+    'T_B4':         list_B4,
+    'T_S11_pred':        list_S11_pred,
+    'T_S21_pred':        list_S21_pred,
+    'T_S31_pred':        list_S31_pred,
+    'T_B1_pred':         list_B1_pred,
+    'T_B4_pred':         list_B4_pred,
+    'elec_cost':    round(elec_cost,2),
+    'elec_used':    round(elec_used/1000,2),
+}
+
+# Print total simulation time
+total_time = time.time()-start_time
+hours = round(total_time // 3600)
+minutes = round((total_time % 3600) // 60)
+print(f"\nThe {simulation_time}-hour simulation ran in {hours} hour(s) and {minutes} minute(s).")
+
+print("\nPlotting the data...\n")
+plot.plot_MPC(plot_data)
